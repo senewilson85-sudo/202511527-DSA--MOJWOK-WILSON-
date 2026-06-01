66db7f89e84bd46ebc01baaf6c59648be752a9e0
senewilson85-sudo <senewilson85@gmail.com>
2026-06-01 22:24:01 +0300

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,15 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+// Allowed libraries only
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Function Declarations
+void writeFile(string filename);
+void readFile(string filename);
+void appendFile(string filename);
+
+#endif
